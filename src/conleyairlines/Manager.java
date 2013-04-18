fd79a522e6e3a3a49a9d46cc07670db03b1d313b
Casey Conley <plazma4545@gmail.com>
2013-04-18 00:36:04 -0400

    Brainstorming of various managers/functions

--- src/conleyairlines/Manager.java
+++ src/conleyairlines/Manager.java
@@ -21,4 +21,69 @@ public class Manager {
         this.con = con;
         this.in = in;
     }
+    
+    public void openInterface(){
+        System.out.println("Hello, please specify who you are.");
+        System.out.println("What action would you like to perform?");
+        printOptions();
+        boolean done = false;
+        while(!done){
+            int userChoice;
+            try{
+                userChoice = in.nextInt();
+                switch(userChoice){
+                    case 1:
+                        System.out.println("Option 1 chosen\n");
+                        done = false;
+                        break;
+                    case 2:
+                        System.out.println("Option 2 chosen\n");
+                        in.nextLine();
+                        done = false;
+                        break;
+                    case 3:
+                        System.out.println("Option 3 chosen\n");
+                        done = false;
+                        break;
+                    case 4:
+                        System.out.println("Option 2 chosen\n");
+                        done = false;
+                        break;
+                    case 5:
+                        System.out.println("Exiting Customer Interface.\n");
+                        done = true;
+                        break;
+                    default:
+                        System.out.println("Error: Please enter a valid number choice.");
+                        in.nextLine();
+                        done = false;
+                        break;
+                }
+            } catch (InputMismatchException ex) {
+                System.out.println("Error: Please enter a valid number choice.");
+                
+                done = false;
+            }
+            catch (IllegalStateException ex) {
+                in = new Scanner(System.in);
+                System.out.println("Error: Internal Error, scanner closed, "
+                        + "reinitialized. \nPlease try again.");
+                
+                done = false;
+            }
+            if(!done){
+                System.out.println("What action would you like to perform?");
+                printOptions();
+            }
+        }
+        
+    }
+    
+    private void printOptions(){
+        System.out.println("1: ");
+        System.out.println("2: ");
+        System.out.println("3: ");
+        System.out.println("4: ");
+        System.out.println("5: ");
+    }
 }
